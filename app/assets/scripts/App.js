7307a0b1ab96bb61e7e49bd826d520f1a2181fbb
Cloudbop <boppincolin@gmail.com>
2020-02-24 23:02:35 +0000

    added sticky header and logo animation transform

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,14 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/revealOnScroll';
-
+import StickyHeader from './modules/StickHeader';
 // let revealOnScoll = new RevealOnScroll();
 
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 75);
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 60);
+
 //
 // alert('Hello World!');
 // make changes if got
---
+++ app/assets/scripts/modules/StickHeader.js
@@ -0,0 +1,20 @@
+import throttle from 'lodash/throttle';
+class StickyHeader {
+  //
+  constructor() {
+    this.siteHeader = document.querySelector('.site-header');
+    this.evts();
+  }
+  evts() {
+    window.addEventListener('scroll', throttle(() => this.runOnScroll(), 200));
+  }
+  runOnScroll() {
+    if (window.scrollY > 60) {
+      this.siteHeader.classList.add('site-header--dark');
+    } else {
+      this.siteHeader.classList.remove('site-header--dark');
+    }
+  }
+}
+
+export default StickyHeader;
