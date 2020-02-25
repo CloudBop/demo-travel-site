e99339f1157495042f42dbb6f21de6f661cd26f9
Cloudbop <boppincolin@gmail.com>
2020-02-25 00:30:46 +0000

    added modal css - js module

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,15 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/revealOnScroll';
 import StickyHeader from './modules/StickHeader';
+import Modal from './modules/Modal';
+
 // let revealOnScoll = new RevealOnScroll();
 
 let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 75);
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 60);
-
+new Modal();
 //
 // alert('Hello World!');
 // make changes if got
